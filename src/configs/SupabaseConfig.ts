e8f08533934401176d1c481b0d7f7f7b007798c4
Ayaan Ahmad <ayaansheikh061@gmail.com>
2025-01-25 03:20:23 +0500

    Update SupabaseConfig.ts

--- src/configs/SupabaseConfig.ts
+++ src/configs/SupabaseConfig.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ytxmcpbuxkdtwqaewdpt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0eG1jcGJ1eGtkdHdxYWV3ZHB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0ODY5NDIsImV4cCI6MjA1MTA2Mjk0Mn0._dT1j14JAq258Zs0OwXhWtCX3TGmgv7leAJA-3SUq2w';
+const supabaseUrl = '';
+const supabaseKey = '';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
